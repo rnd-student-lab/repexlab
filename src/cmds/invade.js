e8c9e03fcbae2f89f93b151c43e9ce8edc895773
Dmitry Ilin <i@dmitryilin.com>
2022-01-08 16:35:06 +0300

    Remove sample commands

--- src/cmds/invade.js
+++
@@ -1,6 +0,0 @@
-export const command = ['invade <command>'];
-export const desc = 'Invade space territory';
-export const builder = yargs => yargs.commandDir('invade_cmds');
-export const handler = argv => {
-  console.log(argv);
-};
